c2dd309aa57b0c4faf1d4b20532192a9ed5d9646
ishiyama-atsushi <ishiyama-a@hss.osaka-u.ac.jp>
2021-08-25 22:23:58 +0900

    変更

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
-import numpy as np
 import cv2
 
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+def main():
+    # 入力画像の読み込み
+    img = cv2.imread("Lenna.jpg")
 
-img = cv2.imread('Lenna.png')
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # カスケード型識別器の読み込み
+    cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
-faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-for (x,y,w,h) in faces:
-    img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-    roi_gray = gray[y:y+h, x:x+w]
-    roi_color = img[y:y+h, x:x+w]
-    eyes = eye_cascade.detectMultiScale(roi_gray)
-    for (ex,ey,ew,eh) in eyes:
-        cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+    # グレースケール変換
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-cv2.imshow('img',img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    # 顔領域の探索
+    face = cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3, minSize=(30, 30))
+
+    # 顔領域を赤色の矩形で囲む
+    for (x, y, w, h) in face:
+        cv2.rectangle(img, (x, y), (x + w, y+h), (0,0,300), 4)
+
+    # 結果を出力
+    cv2.imwrite("result.jpg",img)
+
+
+if __name__ == '__main__':
+    main()
