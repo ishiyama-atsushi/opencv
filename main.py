e22f091b1cb489ea5cf810c2dcc8a58a9e9710b9
ishiyama-atsushi <atsushi990322@gmail.com>
2021-08-25 11:39:48 +0900

    アニメの顔を抜く

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
-import numpy as np
+import os
 import cv2
 
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+# 特徴量ファイルをもとに分類器を作成
+classifier = cv2.CascadeClassifier('lbpcascade_animeface.xml')
 
-img = cv2.imread('Lenna.png')
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# 顔の検出
+image = cv2.imread('320.jpg')
+# グレースケールで処理を高速化
+gray_image = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+faces = classifier.detectMultiScale(gray_image)
+print(faces)
+output_dir = 'faces'
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
 
-faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-for (x,y,w,h) in faces:
-    img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-    roi_gray = gray[y:y+h, x:x+w]
-    roi_color = img[y:y+h, x:x+w]
-    eyes = eye_cascade.detectMultiScale(roi_gray)
-    for (ex,ey,ew,eh) in eyes:
-        cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+for i, (x,y,w,h) in enumerate(faces):
+    # 一人ずつ顔を切り抜く
+    face_image = image[y:y+h, x:x+w]
+    output_path = os.path.join(output_dir,'{0}.jpg'.format(i))
+    cv2.imwrite(output_path,face_image)
 
-cv2.imshow('img',img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+cv2.imwrite('/faces/face.jpg',image)
