840b389b3565ed6706bf8813203b168c93cc4beb
ishiyama-atsushi <ishiyama-a@hss.osaka-u.ac.jp>
2021-08-25 22:30:11 +0900

    修正

--- main.py
+++ main.py
@@ -1,29 +1,3 @@
-import cv2
-
-def main():
-    # 入力画像の読み込み
-    img = cv2.imread("Lenna.jpg")
-
-    # カスケード型識別器の読み込み
-    cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
-
-    # グレースケール変換
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    # 顔領域の探索
-    face = cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3, minSize=(30, 30))
-
-    # 顔領域を赤色の矩形で囲む
-    for (x, y, w, h) in face:
-        cv2.rectangle(img, (x, y), (x + w, y+h), (0,0,300), 4)
-
-    # 結果を出力
-    cv2.imwrite("result.jpg",img)
-
-
-if __name__ == '__main__':
-    main()
-=======
 import os
 import cv2
 
@@ -47,4 +21,3 @@ for i, (x,y,w,h) in enumerate(faces):
     cv2.imwrite(output_path,face_image)
 
 cv2.imwrite('/faces/face.jpg',image)
->>>>>>> 3160156c97aa01396fd236074cdbe5a2cd7f9201
