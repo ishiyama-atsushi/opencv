aa851d797884d193068c2f20ce23bc8b4d8aaebc
ishiyama-atsushi <atsushi990322@gmail.com>
2021-08-26 11:20:45 +0900

    各種保存方法の追加

--- video_save.py
+++ video_save.py
@@ -43,7 +43,7 @@ def save_frame(video_path, frame_num, result_path):
 #save_frame('data/oregairuop.mp4', 500, 'data/temp2/result_single/sample_500.jpg')
 
 
-
+#任意の範囲のフレームを抜いてくるsampleは200から300を10づつ抜いてくる。
 def save_frame_range(video_path, start_frame, stop_frame, step_frame,
                      dir_path, basename, ext='jpg'):
     cap = cv2.VideoCapture(video_path)
@@ -64,6 +64,62 @@ def save_frame_range(video_path, start_frame, stop_frame, step_frame,
         else:
             return
 
-save_frame_range('data/oregairuop.mp4',
-                 200, 300, 10,
-                 'data/temp3/result_range', 'sample_video_img')
+#save_frame_range('data/oregairuop.mp4',
+                 #200, 300, 10,
+                 #'data/temp3/result_range', 'sample_video_img')
+
+#今までのコードはフレームで設定していたが、今回は秒数で指定
+def save_frame_sec(video_path, sec, result_path):
+    cap = cv2.VideoCapture(video_path)
+
+    if not cap.isOpened():
+        return
+
+    os.makedirs(os.path.dirname(result_path), exist_ok=True)
+
+    fps = cap.get(cv2.CAP_PROP_FPS)
+
+    cap.set(cv2.CAP_PROP_POS_FRAMES, round(fps * sec))
+
+    ret, frame = cap.read()
+
+    if ret:
+        cv2.imwrite(result_path, frame)
+
+#save_frame_sec('data/oregairuop.mp4', 10, 'data/temp4/result_10sec.jpg')
+
+#何秒から何秒までという風に区切って保存する。
+def save_frame_range_sec(video_path, start_sec, stop_sec, step_sec,
+                         dir_path, basename, ext='jpg'):
+    cap = cv2.VideoCapture(video_path)
+
+    if not cap.isOpened():
+        return
+
+    os.makedirs(dir_path, exist_ok=True)
+    base_path = os.path.join(dir_path, basename)
+
+    digit = len(str(int(cap.get(cv2.CAP_PROP_FRAME_COUNT))))
+
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    fps_inv = 1 / fps
+
+    sec = start_sec
+    while sec < stop_sec:
+        n = round(fps * sec)
+        cap.set(cv2.CAP_PROP_POS_FRAMES, n)
+        ret, frame = cap.read()
+        if ret:
+            cv2.imwrite(
+                '{}_{}_{:.2f}.{}'.format(
+                    base_path, str(n).zfill(digit), n * fps_inv, ext
+                ),
+                frame
+            )
+        else:
+            return
+        sec += step_sec
+
+#save_frame_range_sec('data/oregairuop.mp4',
+                     #2, 10, 2,#これだと2から10までを2づつ抜く
+                     #'data/temp5/result_range_sec', 'sample_video_img')
